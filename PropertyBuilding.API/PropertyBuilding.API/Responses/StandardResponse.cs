b7c5dd908bc90c50ca35b3b79feec943c03d1264
Ricardo Gonzalez Cubillos <ricardo.gonzalez@msn.com>
2021-09-11 21:06:09 -0500

    Create StandardResponse

---
+++ PropertyBuilding.API/PropertyBuilding.API/Responses/StandardResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PropertyBuilding.API.Responses
+{
+    public class StandardResponse<T>
+    {
+        public T Data { get; set; }
+        public StandardResponse(T data)
+        {
+            Data = data;
+        }
+    }
+}
