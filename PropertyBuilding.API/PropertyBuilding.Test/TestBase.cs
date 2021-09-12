27a64ee1da9926f879fa6fe752ed04cb17b08d90
Ricardo Gonzalez Cubillos <ricardo.gonzalez@msn.com>
2021-09-12 18:49:42 -0500

    Create TestBase.cs

---
+++ PropertyBuilding.API/PropertyBuilding.Test/TestBase.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PropertyBuilding.Core.Interfaces;
+using PropertyBuilding.Infrastructure.Data;
+using PropertyBuilding.Infrastructure.Mappings;
+using PropertyBuilding.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyBuilding.Test
+{
+    public class TestBase
+    {
+        protected IUnitOfWork CreateUnitOfwork(string dataBaseName)
+        {
+            var options = new DbContextOptionsBuilder<PropertyBuildingDataBaseContext>()
+                .UseInMemoryDatabase(dataBaseName).Options;
+            var dataBaseContext = new PropertyBuildingDataBaseContext(options);
+            IUnitOfWork unitOfWork = new UnitOfWork(dataBaseContext);
+            return unitOfWork;
+        }
+
+        protected IMapper ConfigAutoMapper()
+        {
+            var config = new MapperConfiguration(options =>
+            {
+                options.AddProfile(new AutoMapperProfile());
+            });
+
+            return config.CreateMapper();
+        }
+
+        protected IConfiguration CreateConfiguration()
+        {
+            var authenticationConfiguration = new Dictionary<string, string>
+            {
+                {"Authentication:SecretKey", "9zQobBuKXo5yzUC-jb-t2nN4tfHCY80xE6mJEOvVdEk$hKJaid"},
+                {"Authentication:ValidIssuer", "https://localhost:44373/"},
+                {"Authentication:ValidAudience", "https://localhost:44373/"},
+                {"Authentication:LifetimeInMinutes", "25"}
+            };
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(authenticationConfiguration)
+                .Build();
+
+            return configuration;
+        }
+    }
+}
