71fbe02803b9f1e1595cf48ebe103dbeb74b338c
Ricardo Gonzalez Cubillos <ricardo.gonzalez@msn.com>
2021-09-13 21:34:37 -0500

    Add PropertyQueryFilter

---
+++ PropertyBuilding.API/PropertyBuilding.Core/QueryFilters/PropertyQueryFilter.cs
@@ -0,0 +1,16 @@
+﻿namespace PropertyBuilding.Core.QueryFilters
+{
+    public class PropertyQueryFilter : PaginationFilter
+    {
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public double? Price { get; set; }
+        public double? PriceInitial { get; set; }
+        public double? PriceFinal { get; set; }
+        public string? CodeInternal { get; set; }
+        public int? Year { get; set; }
+        public int? YearInitial { get; set; }
+        public int? YearFinal { get; set; }
+        public int? IdOwner { get; set; }
+    }
+}
