de0c41738573fa260bac32694b7c0f26f68ac858
Ricardo Gonzalez Cubillos <ricardo.gonzalez@msn.com>
2021-09-14 11:51:28 -0500

    Add PropertyService
    - Add PropertyService
    - Add PropertyServiceTest

---
+++ PropertyBuilding.API/PropertyBuilding.Core/Services/PropertyService.cs
@@ -0,0 +1,147 @@
+﻿using PropertyBuilding.Core.DTOs;
+using PropertyBuilding.Core.Entities;
+using PropertyBuilding.Core.Exceptions;
+using PropertyBuilding.Core.Interfaces;
+using PropertyBuilding.Core.Options;
+using PropertyBuilding.Core.QueryFilters;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PropertyBuilding.Core.Const.ErrorMessages;
+
+namespace PropertyBuilding.Core.Services
+{
+    public class PropertyService : IPropertyService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IOwnerService _ownerService;
+        private readonly PaginationOptions _paginationOptions;
+        public PropertyService(IUnitOfWork unitOfWork, IOptions<PaginationOptions> paginationOptions, IOwnerService ownerService)
+        {
+            _unitOfWork = unitOfWork;
+            _paginationOptions = (PaginationOptions)paginationOptions.Value;
+            _ownerService = ownerService;
+        }
+        public PagedListDto<Property> GetProperties(PropertyQueryFilter propertyQueryFilter)
+        {
+            var properties = _unitOfWork.PropertyRepository.GetList();
+            if (propertyQueryFilter.Name != null)
+            {
+                properties = properties.Where(item => item.Name.ToLower().Contains(propertyQueryFilter.Name.ToLower()));
+            }
+            if (propertyQueryFilter.Address != null)
+            {
+                properties = properties.Where(item => item.Address.ToLower().Contains(propertyQueryFilter.Address.ToLower()));
+            }
+            if (propertyQueryFilter.CodeInternal != null)
+            {
+                properties = properties.Where(item => item.CodeInternal.ToLower() == propertyQueryFilter.CodeInternal.ToLower());
+            }
+            if (propertyQueryFilter.IdOwner != null)
+            {
+                properties = properties.Where(item => item.IdOwner == propertyQueryFilter.IdOwner);
+            }
+            properties = PriceFilter(properties, propertyQueryFilter);
+            properties = YearFilter(properties, propertyQueryFilter);
+            var propertiesPagedList = PaginationFilter(properties, propertyQueryFilter);
+            return propertiesPagedList;
+        }
+
+        public Task<Property> GetPropertyByIdAsync(int id)
+        {
+            var property = _unitOfWork.PropertyRepository.GetByIdAsync(id);
+            return property;
+        }
+
+        public async Task<Property> ChangePricePropertyAsync(Property changePriceProperty)
+        {
+            var propertyToUpdate = await _unitOfWork.PropertyRepository.GetByIdAsync(changePriceProperty.Id);
+            propertyToUpdate.Price = changePriceProperty.Price;
+            _unitOfWork.PropertyRepository.Update(propertyToUpdate);
+            await _unitOfWork.SaveChangesAsync();
+
+            return propertyToUpdate;
+        }
+        private async Task ValidateOwner(int id)
+        {
+            if (!await _ownerService.ValidateOwner(id))
+            {
+                throw new BusinessException(OwnerErrorMessages.OwnerDoesNotExist);
+            }
+        }
+
+        public async Task<Property> UpdatePropertyAsync(Property property)
+        {
+            await ValidateOwner(property.IdOwner);
+            var propertyOld = await _unitOfWork.PropertyRepository.GetByIdAsync(property.Id);
+            if (propertyOld == null) throw new BusinessException(PropertyErrorMessages.PropertyDoesNotExist);
+            if (propertyOld.IdUser != property.IdUser)
+            {
+                throw new BusinessException(PropertyErrorMessages.PropertyUpdateNotAllowed);
+            }
+            property.Status = propertyOld.Status;
+            _unitOfWork.PropertyRepository.Update(property);
+            await _unitOfWork.SaveChangesAsync();
+            return property;
+        }
+        public async Task<Property> SavePropertyAsync(Property property)
+        {
+            await ValidateOwner(property.IdOwner);
+            if (property.Id.Equals(0))
+            {
+                await _unitOfWork.PropertyRepository.AddAsync(property);
+            }
+            else
+            {
+                var propertyOld = await _unitOfWork.PropertyRepository.GetByIdAsync(property.Id);
+                property.IdUser = propertyOld.IdUser;
+                _unitOfWork.PropertyRepository.Update(property);
+            }
+            await _unitOfWork.SaveChangesAsync();
+            return property;
+        }
+
+        private static IEnumerable<Property> PriceFilter(IEnumerable<Property> properties, PropertyQueryFilter propertyQueryFilter)
+        {
+            if (propertyQueryFilter.Price != null)
+            {
+                properties = properties.Where(item => item.Price == propertyQueryFilter.Price);
+            }
+            else if (propertyQueryFilter.PriceInitial != null && propertyQueryFilter.PriceFinal != null)
+            {
+                properties = properties.Where(item => item.Price >= propertyQueryFilter.PriceInitial && item.Price <= propertyQueryFilter.PriceFinal);
+            }
+
+            return properties;
+        }
+
+        private static IEnumerable<Property> YearFilter(IEnumerable<Property> properties, PropertyQueryFilter propertyQueryFilter)
+        {
+            if (propertyQueryFilter.Year != null)
+            {
+                properties = properties.Where(item => item.Year == propertyQueryFilter.Year);
+            }
+            else if (propertyQueryFilter.YearInitial != null && propertyQueryFilter.YearInitial != null)
+            {
+                properties = properties.Where(item => item.Year >= propertyQueryFilter.YearInitial && item.Price <= propertyQueryFilter.YearInitial);
+            }
+
+            return properties;
+        }
+
+        private PagedListDto<Property> PaginationFilter(IEnumerable<Property> properties, PropertyQueryFilter propertyQueryFilter)
+        {
+            var propertiesPagedList = PagedListDto<Property>.Pagination(properties, propertyQueryFilter.page, propertyQueryFilter.rows, _paginationOptions);
+
+            return propertiesPagedList;
+        }
+
+        public async Task<bool> ValidatePropertyAsync(int id)
+        {
+            return (await _unitOfWork.PropertyRepository.GetByIdAsync(id) != null);
+        }
+    }
+}
---
+++ PropertyBuilding.API/PropertyBuilding.Test/UnitTestServices/PropertyServiceTest.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.Extensions.Options;
+using NUnit.Framework;
+using PropertyBuilding.Core.Entities;
+using PropertyBuilding.Core.Enumerations;
+using PropertyBuilding.Core.Interfaces;
+using PropertyBuilding.Core.Options;
+using PropertyBuilding.Core.QueryFilters;
+using PropertyBuilding.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyBuilding.Test.UnitTestServices
+{
+    [TestFixture]
+    public class PropertyServiceTest : TestBase
+    {
+        private IPropertyService _propertyService;
+        private IOwnerService _ownerService;
+        private IUnitOfWork _unitOfWork;
+        private IOptions<PaginationOptions> _paginationOptions;
+        private async Task<IEnumerable<Property>> MockProperties(int numberOfProperties, int numberOfOwners)
+        {
+            const int MAX_PRICE = 100000;
+            const int MIN_PRICE = 1000;
+            const int MIN_YEAR = 1900;
+            for (int index = 1; index <= numberOfProperties; index++)
+            {
+                int idOwner = new Random().Next(1, numberOfOwners);
+                int year = new Random().Next(MIN_YEAR, 2021);
+                double price = new Random().Next(MIN_PRICE, MAX_PRICE);
+                var propertyToSave = new Property { Name = "Name Test " + index, Address = "Address test", Year = year, Status = StatusType.Active, Price = price, IdOwner = idOwner };
+                await _propertyService.SavePropertyAsync(propertyToSave);
+            }
+            PropertyQueryFilter propertyQueryFilter = new PropertyQueryFilter { rows = numberOfProperties, page = 1 };
+            return _propertyService.GetProperties(propertyQueryFilter);
+        }
+
+        private async Task<IEnumerable<Owner>> MockOwners(int numberOfOwners)
+        {            
+            const int MIN_BIRTHDAY_YEAR = -90;
+            const int MAX_BIRTHDAY_YEAR = -22;
+            for (int index = 1; index <= numberOfOwners; index++)
+            {
+                DateTime birthday = DateTime.Now.AddYears(new Random().Next(MIN_BIRTHDAY_YEAR, MAX_BIRTHDAY_YEAR)).Date;
+                var ownerToSave = new Owner { Name = "Name Owner Test " + index, Address = "Address test", Photo = "Url Photo Test", Birthday = birthday, Status = StatusType.Active };
+                await _ownerService.SaveOwnerAsync(ownerToSave);
+            }
+            return _ownerService.GetOwners();            
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _paginationOptions = Options.Create(new PaginationOptions() { DefaultPageNumber = 5, DefaultPageSize = 1 });
+            var dataBaseName = Guid.NewGuid().ToString();
+            _unitOfWork = CreateUnitOfwork(dataBaseName);
+            _ownerService = new OwnerService(_unitOfWork);
+            _propertyService = new PropertyService(_unitOfWork, _paginationOptions, _ownerService);
+        }
+        
+        [Test]
+        public async Task GetPropertiesWithoutQueryFilterSucess()
+        {
+            var ownerList = await MockOwners(100);
+            var propertyListMock = await MockProperties(100, ownerList.Count());
+            PropertyQueryFilter propertyQueryFilter = new PropertyQueryFilter { rows = 100, page = 1 };
+            var propertyList = _propertyService.GetProperties(propertyQueryFilter);
+            Assert.AreEqual(100, propertyList.Count());
+        }
+
+        [Test]
+        public async Task GetPropertyByIdAsyncSucess()
+        {
+            var ownerList = await MockOwners(20);
+            var propertyListMock = await MockProperties(100, ownerList.Count());
+            var idProperty = new Random().Next(1, 100);
+            var property = await _propertyService.GetPropertyByIdAsync(idProperty);
+            Assert.AreEqual(idProperty, property.Id);
+            Assert.AreEqual("Name Test " + idProperty, property.Name);
+        }
+
+
+        [Test]
+        public async Task ChangePricePropertyAsyncSuccess()
+        {
+            const int MIN_BIRTHDAY_YEAR = -90;
+            const int MAX_BIRTHDAY_YEAR = -22;
+            const int MAX_PRICE = 50000;
+            const int MIN_PRICE = 1000;
+            DateTime birthday = DateTime.Now.AddYears(new Random().Next(MIN_BIRTHDAY_YEAR, MAX_BIRTHDAY_YEAR)).Date;
+            var owner = new Owner { Name = "Name Owner Test", Address = "Address test", Photo = "Url Photo Test", Birthday = birthday, Status = StatusType.Active };
+            await _ownerService.SaveOwnerAsync(owner);            
+            double price = new Random().Next(MIN_PRICE, MAX_PRICE);
+            var propertyToSave = new Property { Name = "Name Test", Address = "Address test", Year = DateTime.Now.Year, Status = StatusType.Active, Price = price, IdOwner = owner.Id };
+            var propertySaved = await _propertyService.SavePropertyAsync(propertyToSave);
+            _unitOfWork.ChangeTrackerClear();
+            double priceToChange = new Random().Next(60000, 100000);
+            Assert.AreEqual(price, propertyToSave.Price);
+            propertySaved.Price = priceToChange;
+            var propertyPriceChanged = await _propertyService.ChangePricePropertyAsync(propertySaved);
+            Assert.IsNotNull(propertyPriceChanged);
+            Assert.AreEqual(propertySaved.Id, propertyPriceChanged.Id);
+            Assert.AreEqual(propertySaved.Price, propertyPriceChanged.Price);
+            Assert.AreEqual(priceToChange, propertyPriceChanged.Price);
+            Assert.AreNotEqual(price, propertyPriceChanged.Price);
+        }
+
+
+        [Test]
+        public async Task UpdatePropertyAsyncSuccess()
+        {
+            const int MIN_BIRTHDAY_YEAR = -90;
+            const int MAX_BIRTHDAY_YEAR = -22;
+            const int MAX_PRICE = 50000;
+            const int MIN_PRICE = 1000;
+            DateTime birthday = DateTime.Now.AddYears(new Random().Next(MIN_BIRTHDAY_YEAR, MAX_BIRTHDAY_YEAR)).Date;
+            var owner = new Owner { Name = "Name Owner Test", Address = "Address test", Photo = "Url Photo Test", Birthday = birthday, Status = StatusType.Active };
+            await _ownerService.SaveOwnerAsync(owner);
+            double price = new Random().Next(MIN_PRICE, MAX_PRICE);
+            var propertyToSave = new Property { Name = "Name Test", Address = "Address test", Year = DateTime.Now.Year, Status = StatusType.Active, Price = price, IdOwner = owner.Id };
+            var propertySaved = await _propertyService.SavePropertyAsync(propertyToSave);
+            _unitOfWork.ChangeTrackerClear();
+            var propertyToChange = new Property { Name = "New Name", Address = "New Address test", Year = DateTime.Now.AddYears(-5).Year, Status = StatusType.Active, Price = price, IdOwner = owner.Id };
+            propertyToChange.Id = propertySaved.Id;
+            var propertyChanged = await _propertyService.UpdatePropertyAsync(propertyToChange);
+            Assert.IsNotNull(propertyChanged);
+            Assert.AreEqual(propertySaved.Id, propertyChanged.Id);
+            Assert.AreNotEqual(propertySaved.Name, propertyChanged.Name);
+            Assert.AreNotEqual(propertySaved.Address, propertyChanged.Address);
+            Assert.AreNotEqual(propertySaved.Year, propertyChanged.Year);           
+        }
+
+
+        [Test]
+        public async Task SavePropertyAsync()
+        {
+            const int MIN_BIRTHDAY_YEAR = -90;
+            const int MAX_BIRTHDAY_YEAR = -22;
+            const int MAX_PRICE = 50000;
+            const int MIN_PRICE = 1000;
+            const string NAME = "Name Test";
+            const string ADDRESS = "Address Test";
+            DateTime birthday = DateTime.Now.AddYears(new Random().Next(MIN_BIRTHDAY_YEAR, MAX_BIRTHDAY_YEAR)).Date;
+            var owner = new Owner { Name = "Name Owner Test", Address = ADDRESS, Photo = "Url Photo Test", Birthday = birthday, Status = StatusType.Active };
+            await _ownerService.SaveOwnerAsync(owner);
+            double price = new Random().Next(MIN_PRICE, MAX_PRICE);
+            var propertyToSave = new Property { Name = NAME, Address = ADDRESS, Year = DateTime.Now.Year, Status = StatusType.Active, Price = price, IdOwner = owner.Id };
+            var propertySaved = await _propertyService.SavePropertyAsync(propertyToSave);
+            Assert.IsNotNull(propertySaved);
+            Assert.AreEqual(1, propertySaved.Id);
+            Assert.AreEqual(NAME, propertySaved.Name);
+            Assert.AreEqual(ADDRESS, propertySaved.Address);
+            Assert.AreEqual(DateTime.Now.Year, propertySaved.Year);
+            Assert.AreEqual(owner.Id, propertySaved.IdOwner);
+        }
+        [Test]
+        public async Task ValidatePropertyAsyncSuccess()
+        {
+            const int MIN_BIRTHDAY_YEAR = -90;
+            const int MAX_BIRTHDAY_YEAR = -22;
+            const int MAX_PRICE = 50000;
+            const int MIN_PRICE = 1000;
+            const string NAME = "Name Test";
+            const string ADDRESS = "Address Test";
+            DateTime birthday = DateTime.Now.AddYears(new Random().Next(MIN_BIRTHDAY_YEAR, MAX_BIRTHDAY_YEAR)).Date;
+            var owner = new Owner { Name = "Name Owner Test", Address = ADDRESS, Photo = "Url Photo Test", Birthday = birthday, Status = StatusType.Active };
+            await _ownerService.SaveOwnerAsync(owner);
+            double price = new Random().Next(MIN_PRICE, MAX_PRICE);
+            var propertyToSave = new Property { Name = NAME, Address = ADDRESS, Year = DateTime.Now.Year, Status = StatusType.Active, Price = price, IdOwner = owner.Id };
+            var propertySaved = await _propertyService.SavePropertyAsync(propertyToSave);
+            bool propertyIsValid = await _propertyService.ValidatePropertyAsync(propertyToSave.Id);
+            Assert.IsNotNull(propertyIsValid);
+            Assert.AreEqual(true, propertyIsValid);            
+        }
+    }
+}
