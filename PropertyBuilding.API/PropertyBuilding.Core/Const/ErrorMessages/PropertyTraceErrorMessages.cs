e975e8f8703a0f203838688202554e298ef554b2
Ricardo Gonzalez Cubillos <ricardo.gonzalez@msn.com>
2021-09-15 12:25:28 -0500

    Add PropertyTraceErrorMessages

---
+++ PropertyBuilding.API/PropertyBuilding.Core/Const/ErrorMessages/PropertyTraceErrorMessages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyBuilding.Core.Const.ErrorMessages
+{
+    public class PropertyTraceErrorMessages
+    {
+        public const string DateSaleCannotThanGreaterToday = "Date sale cannot than greater today";
+    }
+}
