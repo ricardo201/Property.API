e5ca84bd60e5a7944cb19b54d9c806c7bae09435
Ricardo Gonzalez Cubillos <ricardo.gonzalez@msn.com>
2021-09-14 23:28:32 -0500

    Add PropertyImageRepository
    - Add IPropertyImageRepository
    - Add PropertyImageRepository

---
+++ PropertyBuilding.API/PropertyBuilding.Infrastructure/Repositories/PropertyImageRepository.cs
@@ -0,0 +1,17 @@
+﻿using PropertyBuilding.Core.Entities;
+using PropertyBuilding.Core.Interfaces;
+using PropertyBuilding.Infrastructure.Data;
+using System.Linq;
+
+namespace PropertyBuilding.Infrastructure.Repositories
+{
+    public class PropertyImageRepository : Repository<PropertyImage>, IPropertyImageRepository
+    {
+        public PropertyImageRepository(PropertyBuildingDataBaseContext dataBaseContext) : base(dataBaseContext)
+        { }
+        public PropertyImage GetPropertyImageByName(string fileName, int idUser)
+        {
+            return _entities.FirstOrDefault(item => item.File == fileName && item.IdUser == idUser && item.Enabled == true);
+        }
+    }
+}
