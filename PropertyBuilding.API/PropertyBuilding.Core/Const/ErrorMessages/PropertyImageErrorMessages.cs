8c091931532a0368084d7c0742a2e579c185640c
Ricardo Gonzalez Cubillos <ricardo.gonzalez@msn.com>
2021-09-14 23:02:18 -0500

    Add PropertyImageErrorMessages

---
+++ PropertyBuilding.API/PropertyBuilding.Core/Const/ErrorMessages/PropertyImageErrorMessages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyBuilding.Core.Const.ErrorMessages
+{
+    public class PropertyImageErrorMessages
+    {
+        public const string ContentType = "Invalid image format";
+        public const string SizeLimit = "Image exceeds the size limit";
+        public const string FileIsRequired = "File is required";
+        public const string FileCannotNull = "File cannot be null";
+        public const string EnableIsRequired = "Field Enable is required";
+        public const string ImageNotFound = "Property image not found";
+    }
+}
