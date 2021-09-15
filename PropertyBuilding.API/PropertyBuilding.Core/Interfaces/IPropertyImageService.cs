b387624df02cfd6c75b99e71b30a6bba67b9855f
Ricardo Gonzalez Cubillos <ricardo.gonzalez@msn.com>
2021-09-15 00:08:16 -0500

    Add PropertyImageService
    - Add IPropertyImageService
    - Add PropertyImageService
    - Add PropertyImageServiceTest

---
+++ PropertyBuilding.API/PropertyBuilding.Core/Interfaces/IPropertyImageService.cs
@@ -0,0 +1,16 @@
+﻿using PropertyBuilding.Core.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PropertyBuilding.Core.Interfaces
+{
+    public interface IPropertyImageService
+    {
+        Task<bool> DeletePropertyImageAsync(int id);
+        IEnumerable<PropertyImage> GetPropertyImages();
+        Task<PropertyImage> SavePropertyImageAsync(PropertyImage PropertyImage);
+        Task<PropertyImage> GetPropertyImageByIdAsync(int id, int idUser);
+        PropertyImage GetPropertyImageByFileName(string fileName, int idUser);
+        Task<PropertyImage> ChangeEnabledPropertyImageAsyn(PropertyImage changeStatePropertyImage, int idUser);
+    }
+}
